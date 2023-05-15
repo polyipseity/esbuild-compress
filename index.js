303f703f0289e8bca740c9249548bae1025a4df0
William So <polyipseity@gmail.com>
2023-05-15 10:53:16 +0800

    Improve code in `index.js`
    Signed-off-by: William So <polyipseity@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { escapeRegExp } from "lodash-es"
 import lzString from "lz-string"
 import { readFile } from "fs/promises"
 
+const { compressToBase64 } = lzString
+
 /**
  * @typedef {object} Options
  * @property {boolean=} lazy whether decompression is lazy
@@ -16,42 +18,37 @@ import { readFile } from "fs/promises"
  * @returns {import("esbuild").Plugin} an esbuild plugin
  */
 export default function esbuildCompress(options = {}) {
+	const { lazy } = options
 	return {
 		name: "compress",
-		setup(build) {
-			function str(string) {
-				if (typeof string !== "string") {
-					throw new TypeError(string)
-				}
-				return `\`${string.replace(/(?<char>`|\\|\$)/ug, "\\$<char>")}\``
-			}
-			const loaders = build.initialOptions.loader ?? {}
+		setup({ initialOptions: { loader } }) {
+			const loaders = loader ?? {}
 			for (const [ext, loader] of Object.entries(loaders)) {
-				const filter = () => new RegExp(`${escapeRegExp(ext)}$`, "u")
+				const filter = new RegExp(`${escapeRegExp(ext)}$`, "u")
 				if (loader === "compressed-text") {
-					build.onLoad({ filter: filter() }, async ({ path }) => {
+					build.onLoad({ filter }, async ({ path }) => {
 						const data = await readFile(path, { encoding: "utf-8" })
 						return {
-							contents: options.lazy ? `
+							contents: lazy ? `
 import PLazy from "p-lazy"
 import { decompressFromBase64 as decompress } from "lz-string"
 export default PLazy.from(() =>
-	decompress(${str(lzString.compressToBase64(data))}))
+	decompress(${jsString(compressToBase64(data))}))
 ` : `
 import { decompressFromBase64 as decompress } from "lz-string"
-export default decompress(${str(lzString.compressToBase64(data))})
+export default decompress(${jsString(compressToBase64(data))})
 `,
 							loader: "js",
 						}
 					})
 				} else if (loader === "compressed-json") {
-					build.onLoad({ filter: filter() }, async ({ path }) => {
+					build.onLoad({ filter }, async ({ path }) => {
 						const data = await readFile(path, { encoding: "utf-8" })
 						JSON.parse(data)
 						return {
 							contents: `
 import { decompressFromBase64 as decompress } from "lz-string"
-export default JSON.parse(decompress(${str(lzString.compressToBase64(data))}))
+export default JSON.parse(decompress(${jsString(compressToBase64(data))}))
 `,
 							loader: "js",
 						}
@@ -63,4 +60,14 @@ export default JSON.parse(decompress(${str(lzString.compressToBase64(data))}))
 			}
 		},
 	}
+}
+
+/**
+ * Escapes a string for embedding in JavaScript.
+ * 
+ * @param {string} string a string
+ * @returns {string} a JavaScript string
+ */
+function jsString(string) {
+	return `\`${string.replace(/(?<char>`|\\|\$)/ug, "\\$<char>")}\``
 }
